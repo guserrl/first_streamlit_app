38019c7dde2b5b5b270c7552930c2f17a9308d87
guserrl <72491192+guserrl@users.noreply.github.com>
2023-10-03 08:38:29 +0200

    Update streamlit_app.py
    header y texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Croquetas')
+streamlit.text('Tortilla patata')
+streamlit.text('Canelones')
